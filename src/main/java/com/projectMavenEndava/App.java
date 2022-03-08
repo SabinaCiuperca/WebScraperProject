ea08d6aba7a3a87460319ce1021435be60dd2034
40746 <c.sabina902@yahoo.com>
2022-03-08 19:13:29 +0200

    project

---
+++ src/main/java/com/projectMavenEndava/App.java
@@ -0,0 +1,13 @@
+package com.projectMavenEndava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
